d169c4286dbda9f627f8f7e95f596c06a7a8142f
Jesuar López <jesdav18@gmail.com>
2022-12-21 15:58:00 -0600

    Agregando funcionalidad para manejar excepciones

---
+++ Nagaira.Core.Extensions/Exceptions/MessageException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Nagaira.Core.Extentions.Exceptions
+{
+    public static class MessageException
+    {
+        public static string LanzarExcepcion(Exception exception)
+        {
+            if (exception.InnerException != null) return new string(exception.InnerException.Message);            
+            return new string(exception.Message);       
+        }
+    }
+}
