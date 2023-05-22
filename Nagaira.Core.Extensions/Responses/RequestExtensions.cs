4373a004b36386c6e61ff4e7f95bb62996944ea0
Jesuar López <jesdav18@gmail.com>
2023-05-22 03:51:31 -0600

    Change to response with Data

--- Nagaira.Core.Extensions/Responses/RequestExtensions.cs
+++ Nagaira.Core.Extensions/Responses/RequestExtensions.cs
@@ -22,11 +22,11 @@
 
     public class Response<TData>
     {
-        public static Response<TData> Ok(TypeResponse type, string message, TData? data = default) => new Response<TData> { Type = type, Message = message, Data = data };
-        public static Response<TData> Exception(TypeResponse type, string message, TData? data = default) => new Response<TData> { Type = type, Message = message, Data = data };
-        public static Response<TData> Warning(TypeResponse type, string message, TData? data = default) => new Response<TData> { Type = type, Message = message, Data = data };
-        public static Response<TData> EntityWarning(TypeResponse type, string message, TData? data = default) => new Response<TData> { Type = type, Message = message, Data = data };
-        public static Response<TData> NotFound(TypeResponse type, string message, TData? data = default) => new Response<TData> { Type = type, Message = message, Data = data };
+        public static Response<TData> Ok(string message, TData? data = default) => new Response<TData> { Type = TypeResponse.Ok, Message = message, Data = data };
+        public static Response<TData> Exception(string message, TData? data = default) => new Response<TData> { Type = TypeResponse.Exception, Message = message, Data = data };
+        public static Response<TData> Warning(string message, TData? data = default) => new Response<TData> { Type = TypeResponse.Warning, Message = message, Data = data };
+        public static Response<TData> EntityWarning(string message, TData? data = default) => new Response<TData> { Type = TypeResponse.EntityWarning, Message = message, Data = data };
+        public static Response<TData> NotFound(string message, TData? data = default) => new Response<TData> { Type = TypeResponse.NotFound, Message = message, Data = data };
         public TypeResponse Type { get; set; }
         public string? Message { get; set; }
         public TData? Data { get; set; }
